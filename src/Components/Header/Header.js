649fabdc3d8581c9f486113339062ca0b9a7c886
Armando Pérez <armper.95@gmail.com>
2021-05-11 20:53:20 -0500

    Sitio web de voltic 12 de mayo

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,23 @@
+import logo from "../../logo-web.svg"
+
+export default function Header () {
+
+    const title = (ev) => {
+        document.title = "Voltic | " + ev.target.innerText
+    }
+
+    return <header className="bg-voltic-blue-900 py-2.5 px-2 md:px-0">
+        <div className="container mx-auto flex justify-between items-center">
+            <figure>
+                <img src={logo} alt="" width="150"/>
+            </figure>
+            <nav className="hidden md:block text-white font-bold">
+                <a className="p-4 hover:text-voltic-red-500" onClick={title} href="#inicio">Inicio</a>
+                <a className="p-4 hover:text-voltic-red-500" onClick={title} href="#nosotros">Nosotros</a>
+                <a className="p-4 hover:text-voltic-red-500" onClick={title} href="#experiencia">Experiencia</a>
+                <a className="p-4 hover:text-voltic-red-500" onClick={title} href="#usuarios">Usuarios</a>
+                <a className="p-4 hover:text-voltic-red-500" onClick={title} href="#contacto">Contacto</a>
+            </nav>
+        </div>
+    </header>
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,51 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            height: {
+                    'slider': '480px',
+            },
+            colors:{
+                voltic:{
+                    blue: {
+                        '50':  '#e8ecee',
+                        '100': '#d2dadd',
+                        '200': '#a5b4ba',
+                        '300': '#778f98',
+                        '400': '#4a6975',
+                        '500': '#1d4453',
+                        '600': '#1a3d4b',
+                        '700': '#14303a',
+                        '800': '#0f222a',
+                        '900': '#091419',
+                    },
+                    red: {
+                        '50':  '#fce6e9',
+                        '100': '#f9cdd2',
+                        '200': '#f49ba5',
+                        '300': '#ee6978',
+                        '400': '#e9374b',
+                        '500': '#e3051e',
+                        '600': '#cc051b',
+                        '700': '#9f0415',
+                        '800': '#72030f',
+                        '900': '#440109',
+                    },
+                    gray:{
+                        '50':  '#e8e8e8',
+                        '100': '#d2d2d1',
+                        '200': '#a5a5a4',
+                        '300': '#777776',
+                        '400': '#4a4a49',
+                        '500': '#1d1d1b',
+                    },
+                },
+            },
+        },
+    },
+    variants: {
     extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    },
+    plugins: [],
 }
